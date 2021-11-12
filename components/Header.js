f01339a1a14725abcb0c379468efeca6350de4dd
volenzo <volenzoclara@gmail.com>
2021-11-12 11:22:46 +0300

    more changes

--- components/Header.js
+++ components/Header.js
@@ -1,9 +1,9 @@
 import Link from "next/link";
-
+import styles from "../styles/Header.module.css";
 export default function Header() {
   return (
-    <header>
-      <div>
+    <header className={styles.header}>
+      <div className={styles.logo}>
         <Link href="/">
           <a>Femi Events</a>
         </Link>
--- components/Layout.js
+++ components/Layout.js
@@ -1,8 +1,12 @@
 import Head from "next/head";
+import {useRouter} from "next/router";
 import Header from "./Header";
+import Footer from "./Footer";
 import styles from "../styles/Layout.module.css";
+import ShowCase from "./ShowCase";
 
 export default function Layout({ title, keywords, description, children }) {
+  const router = useRouter();
   return (
     <div>
       <Head>
@@ -10,9 +14,10 @@ export default function Layout({ title, keywords, description, children }) {
         <meta name="description" content={description} />
         <meta name="keywords" content={keywords} />
       </Head>
-
       <Header />
+      {router.pathname === "/" && <ShowCase/>}
       <div className={styles.container}>{children}</div>
+      <Footer />
     </div>
   );
 }
